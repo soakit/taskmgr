ec9fc73f82a4ed3a20d4e706acd7df8074bad863
soakit <liuchang2024@gmail.com>
2019-06-17 17:02:44 +0800

    统一处理menu和dialog的黑夜模式

--- src/app/project/new-project/new-project.component.ts
+++ src/app/project/new-project/new-project.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core'
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material'
-import { OverlayContainer } from '@angular/cdk/overlay'
 
 @Component({
   selector: 'app-new-project',
@@ -10,15 +9,11 @@ import { OverlayContainer } from '@angular/cdk/overlay'
 export class NewProjectComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) private data,
-    private dialogRef: MatDialogRef<NewProjectComponent>,
-    private oc: OverlayContainer
+    private dialogRef: MatDialogRef<NewProjectComponent>
   ) {}
 
   ngOnInit() {
     console.log(JSON.stringify(this.data))
-    this.oc.getContainerElement().className = this.data.dark
-      ? 'myapp-dark-theme'
-      : ''
   }
 
   onSave() {
